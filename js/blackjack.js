28a655175cdf6fd6b8c7d4fc7492596efe52ff3c
Visaal Patel <visaalpatel@Visaals-MacBook-Pro.local>
2019-02-19 21:26:02 +0000

    add basic dealing and score logic

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,4 +1,42 @@
 // Supporting functions
+function getCardScore(cardValue) {
+  switch (cardValue) {
+    case "Ace":
+    case "King":
+    case "Queen":
+    case "Jack":
+    case "ten":
+      return 10;
+      break;
+    case "nine":
+      return 9;
+      break;
+    case "eight":
+      return 8;
+      break;
+    case "seven":
+      return 7;
+      break;
+    case "six":
+      return 6;
+      break;
+    case "five":
+      return 5;
+      break;
+    case "four":
+      return 4;
+      break;
+    case "three":
+      return 3;
+      break;
+    case "two":
+      return 2;
+    case "one":
+      return 1;
+      break;
+  }
+}
+
 function createDeck() {
   console.log("creating a new deck.");
   hitMeButton.style.display = "inline";
@@ -26,7 +64,8 @@ function createDeck() {
     for (n = 0; n < cardValues.length; n++) {
       deck.push({
         suit: suits[i],
-        value: cardValues[n]
+        value: cardValues[n],
+        score: getCardScore(cardValues[n])
       });
     }
   }
@@ -49,6 +88,38 @@ function shuffleDeck(cardDeck) {
   return shuffledDeck;
 }
 
+function printCard(card) {
+  gameText.innerHTML = "Card dealt is the " + card.value + " of " + card.suit;
+}
+
+function dealCard(cardDeck) {
+  return cardDeck.pop();
+}
+
+function setUpPlayers() {
+  let players = {
+    dealer: [],
+    player1: []
+  };
+  return players;
+}
+
+function initialDeal(players, cardDeck) {
+  for (i = 0; i < 2; i++) {
+    players.player1.push(dealCard(cardDeck));
+    players.dealer.push(dealCard(cardDeck));
+  }
+  return players;
+}
+
+function calculateScore(playerCards) {
+  total = 0;
+  for (i = 0; i < playerCards.length; i++) {
+    total += playerCards[i].score;
+  }
+  return total;
+}
+
 // Show all the in game commentry
 let gameText = document.getElementById("gameText");
 
@@ -57,6 +128,16 @@ newGameButton.addEventListener("click", function() {
   cards = createDeck();
   cards = shuffleDeck(cards);
   gameText.innerHTML = "New game has now started.";
+  players = setUpPlayers();
+  gameText.innerHTML += "<p>Dealing cards</p>";
+  playerCards = initialDeal(players, cards);
+  dealerCards = playerCards.dealer;
+  player1Cards = playerCards.player1;
+  dealerScore = calculateScore(dealerCards);
+  gameText.innerHTML += "<p>Dealer has " + dealerScore + "</p>";
+  playerScore = calculateScore(player1Cards);
+  gameText.innerHTML += "<p>Player has " + playerScore + "</p>";
+  gameText.innerHTML += "<p>What would you like to do?</p>";
 });
 
 let shuffleCardsButton = document.getElementById("shuffleCardsButton");
@@ -68,3 +149,21 @@ let hitMeButton = document.getElementById("hitMeButton");
 let stickButton = document.getElementById("stickButton");
 hitMeButton.style.display = "none";
 stickButton.style.display = "none";
+
+hitMeButton.addEventListener("click", function() {
+  newCard = dealCard(cards);
+  players.player1.push(newCard);
+  playerScore = calculateScore(player1Cards);
+  gameText.innerHTML += "<p>Player now has " + playerScore + "</p>";
+});
+
+stickButton.addEventListener("click", function() {
+  while (dealerScore < 18) {
+    console.log(dealerCards);
+    gameText.innerHTML += "<p>Dealer is taking a card</p>";
+    newCard = dealCard(cards);
+    players.dealer.push(newCard);
+    dealerScore = calculateScore(dealerCards);
+    gameText.innerHTML += "<p>Dealer now has " + dealerScore + "</p>";
+  }
+});
